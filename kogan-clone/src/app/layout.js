3abe4f9c202c67b593558b8837b8b7a21c783ae9
Timothy <timmytam10@gmail.com>
2024-10-12 16:36:54 +1100

    update carousel

--- kogan-clone/src/app/layout.js
+++ kogan-clone/src/app/layout.js
@@ -9,9 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='bg-white'>
-        <NavBar /> 
-        <main >{children}</main> 
+      <body className="bg-white"> {/* Added background color for better contrast */}
+        <NavBar />
+        <div className="container mx-auto px-4 py-6"> {/* Added container with padding */}
+          <main className="  mx-auto"> {/* Set max-width and center align */}
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
--- kogan-clone/src/components/product/Carousel.js
+++ kogan-clone/src/components/product/Carousel.js
@@ -6,7 +6,7 @@ const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlePrevClick = () => {
-    const prevIndex = (currentIndex - 1 + images.length) % images.length;
+    const prevIndex = (currentIndex - 1 + images.length) %images.length;
     setCurrentIndex(prevIndex);
   };
 
@@ -16,25 +16,33 @@ const Carousel = ({ images }) => {
   };
 
   return (
-    <div>
-      <Image
-        src={images[currentIndex]} // Display the current image
-        alt={`Product Image ${currentIndex + 1}`}
-        width={600}
-        height={400}
-        className="w-full"
-      />
+  <div className="max-w-md mx-auto">
+      <div className="relative overflow-hidden">
+        <div className="flex transition-transform ease-out duration-500" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+          {images.map((src, index) => (
+            <Image
+              key={index}
+              src={src}
+              alt={`Product Image ${index + 1}`}
+              width={1000}
+              height={1000}
+              className="w-full flex-shrink-0"
+            />
+          ))}
+        </div>
+      </div>
       <div className="flex justify-center mt-2 space-x-2">
         <button
-          className="p-2 gray-400 bg-gray-400 text-white "
+          className="p-2 bg-gray-400 text-white"
           onClick={handlePrevClick}
         >
-            <Image
-         src='/assets/left-arrow-2.png'
-         width="15"
-         height="21"
-          className="filter invert"
-         /> {/* Left Arrow */}
+          <Image
+            src='/assets/left-arrow-2.png'
+            width="15"
+            height="21"
+            alt="Previous"
+            className="filter invert"
+          />
         </button>
         {images.map((src, index) => (
           <Image
@@ -48,16 +56,16 @@ const Carousel = ({ images }) => {
           />
         ))}
         <button
-          className="p-2 gray-400 bg-gray-400 text-white "
+          className="p-2 bg-gray-400 text-white mr-2"
           onClick={handleNextClick}
         >
-         <Image
-         src='/assets/right-arrow.png'
-         width="15"
-         height="21"
-         className="filter invert"
-         />
-         {/* Right Arrow */}
+          <Image
+            src='/assets/right-arrow.png'
+            width="15"
+            height="21"
+            alt="Next"
+            className="filter invert"
+          />
         </button>
       </div>
     </div>
